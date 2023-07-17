625c038c28dfc03bce6be17ea102631224d9806e
Camilo <camilocraft08@gmail.com>
2023-07-17 13:42:09 -0400

    Added validators to users routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,12 +7,31 @@ const authMiddleware = require('../middleware/authMiddleware');
 router.get('/profile', authMiddleware, userController.getUserProfile);
 
 // POST /users/register - Register a new user
-router.post('/register', userController.registerUser);
+router.post('/register', 
+  [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('email').notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email format'),
+    body('password').notEmpty().withMessage('Password is required').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  ],
+  userController.registerUser
+);
 
 // POST /users/login - User login
-router.post('/login', userController.loginUser);
+router.post('/login',
+  [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('password').notEmpty().withMessage('Password is required'),
+  ],
+  userController.loginUser
+);
 
 // PUT /users/profile - Update user profile
-router.put('/profile', authMiddleware, userController.updateUserProfile);
+router.put('/profile', authMiddleware, 
+  [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('email').notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email format'),
+  ],
+  userController.updateUserProfile
+);
 
 module.exports = router;
