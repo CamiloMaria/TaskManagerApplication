c0ebf108aac81f59f715047ea6bd1bb7fee08044
Camilo <camilocraft08@gmail.com>
2023-07-17 13:42:37 -0400

    Added the code for authMiddleware.js

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  // Get token from the request headers
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Authorization token is missing' });
+  }
+
+  try {
+    // Verify and decode the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.userId;
+    next();
+    
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
