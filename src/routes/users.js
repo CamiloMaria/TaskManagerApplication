f89edd929d6acd7bed573368a2c52ef24b0a4f99
Camilo <camilocraft08@gmail.com>
2023-07-17 13:36:23 -0400

    Added to user.js the missing routers

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 
-router.get('/', userController.getAllUsers);
+// GET /users/profile - Get user profile
+router.get('/profile', authMiddleware, userController.getUserProfile);
+
+// POST /users/register - Register a new user
+router.post('/register', userController.registerUser);
+
+// POST /users/login - User login
+router.post('/login', userController.loginUser);
+
+// PUT /users/profile - Update user profile
+router.put('/profile', authMiddleware, userController.updateUserProfile);
 
 module.exports = router;
