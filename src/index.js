e519398b169d0200744489b581c9f0485452203b
Camilo <camilocraft08@gmail.com>
2023-07-17 13:45:03 -0400

    added the routes for tasks and users in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorMiddleware');
+const userRoutes = require('./routes/users');
+const taskRoutes = require('./routes/tasks');
 
 dotenv.config();
 
@@ -11,13 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
+app.use('/tasks', taskRoutes);
+app.use('/users', userRoutes);
 
 app.use(errorHandler); // Error handling middleware
-
 
 const PORT = process.env.PORT || 3000;
 
