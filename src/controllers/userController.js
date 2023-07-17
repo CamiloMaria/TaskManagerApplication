aeec97a6142a739f5cec4a27c0d3e26d4e4e884b
Camilo <camilocraft08@gmail.com>
2023-07-17 13:58:44 -0400

    changed location of registerUser and loginUser to authController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,58 +2,6 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const registerUser = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-
-    let user = await User.findOne({ $or: [{ username }, { email }] });
-    if (user) {
-      return res.status(400).json({ error: 'User already exists' });
-    }
-
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    user = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    await user.save();
-
-    res.status(201).json({ message: 'User registered successfully' });
-
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-const loginUser = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.status(400).json({ error: 'Invalid credentials' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: 'Invalid credentials' });
-    }
-
-    // Generate JWT token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    res.json({ token });
-
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
 const getUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select('-password');
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
+
 
 // GET /users/profile - Get user profile
 router.get('/profile', authMiddleware, userController.getUserProfile);
@@ -13,7 +15,7 @@ router.post('/register',
     body('email').notEmpty().withMessage('Email is required').isEmail().withMessage('Invalid email format'),
     body('password').notEmpty().withMessage('Password is required').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
   ],
-  userController.registerUser
+  
 );
 
 // POST /users/login - User login
@@ -22,7 +24,6 @@ router.post('/login',
     body('username').notEmpty().withMessage('Username is required'),
     body('password').notEmpty().withMessage('Password is required'),
   ],
-  userController.loginUser
 );
 
 // PUT /users/profile - Update user profile
